8b50accbd4144ebbf449a1c1a219a8c70e50acdf
Abam2442 <134880637+Abam2442@users.noreply.github.com>
2023-10-17 16:22:22 +0300

    j

---
+++ app/Console/Commands/ShowFilesInFolder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+
+class ShowFilesInFolder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:show-files-in-folder';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        print_r(scandir('public/storage'));
+    }
+}
